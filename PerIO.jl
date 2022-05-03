617911bc2851ada258221a76d6fdc50d0887027b
watsonkh <watsonkh@vcu.edu>
2022-05-03 12:19:03 -0400

    simple harmonic oscillator

--- PerIO.jl
+++ PerIO.jl
@@ -1,5 +1,30 @@
-using TOML
 
-input = TOML.parsefile("input.toml")
-print(input["Grid"])
-# input["BoundaryCondition"][1]["include"]
+using TOML
+import CSV
+import NearestNeighbors
+
+# include("Perijules.jl")
+
+input = TOML.parsefile("example_input.toml")
+
+
+
+# nodes::Vector{Node} = []
+
+# Read in all grids
+for input_grid_object in input["Grid"]
+    input_grid = CSV.File(input_grid_object["path"], stripwhitespace=true,  comment="#")
+
+    # Required columns in grid file
+    @assert :x in input_grid.names
+    @assert :y in input_grid.names
+    @assert :z in input_grid.names
+    
+    positions = convert(Matrix{Float64}, vcat(transpose(input_grid[:x]), transpose(input_grid[:y]), transpose(input_grid[:z])))
+    tree = NearestNeighbors.BallTree(positions)
+    result = NearestNeighbors.inrange(tree, positions, 3.014, false)
+    println(typeof(result))
+    n_points::Int64 = input_grid.rows
+end
+
+println("Ran successfully :)")
--- Perijules.jl
+++ Perijules.jl
@@ -1,29 +1,50 @@
 import Pkg
 Pkg.activate(".")
 
-import StaticArrays
+using StaticArrays
 import PyCall
+import NearestNeighbors
+using LinearAlgebra
+
+
+# Global (for now) varibles
+grid_spacing = 2.
+horizon = grid_spacing * 3.014
 
 mutable struct Material
+    id::Int64
     density::Float64
     critical_strain::Float64
-    elastic_modulus::Float64
-    grid_spacing::Float64
-    horizon::Float64
+    bond_constant::Float64
 end
 
 mutable struct Node
-    position::StaticArrays.SVector{3, Float64}
-    displacement::StaticArrays.SVector{3, Float64}
-    velocity::StaticArrays.SVector{3, Float64}
-    force::StaticArrays.SVector{3, Float64}
+    position::MVector{3, Float64}
+    displacement::MVector{3, Float64}
+    velocity::MVector{3, Float64}
+    force::MVector{3, Float64}
+    volume::Float64
     material::Material
 end
+# Some constructors
+Node(x::Float64, y::Float64, z::Float64, m::Material) = Node([x,y,z], [0,0,0], [0,0,0], [0,0,0], grid_spacing^3, m)
+Node(pos::MVector{3, Float64}, m::Material) = Node(pos, [0,0,0], [0,0,0], [0,0,0], grid_spacing^3, m)
+
 
 mutable struct Bond
     from::Node
     to::Node
     isBroken::Bool
+end
+
+function apply_force(bond::Bond)
+    initial_bond_length = norm(bond.to.position - bond.from.position)
+    deformed_bond_vector = (bond.to.position + bond.to.displacement) - (bond.from.position + bond.from.displacement)
+    deformed_bond_length = norm(deformed_bond_vector)
+    strain = (deformed_bond_length - initial_bond_length) / initial_bond_length
+    direction = deformed_bond_vector / deformed_bond_length
+
+    bond.from.force = minimum([bond.from.material.bond_constant, bond.to.material.bond_constant]) * strain * direction * bond.to.volume * bond.from.volume
 end
 
 mutable struct Grid
@@ -33,81 +54,67 @@ mutable struct Grid
     materials::Vector{Material}
 end
 
-function parse_grid(filepath::String, skiprow::Bool)
-    np = PyCall.pyimport("numpy")
-    if skiprow
-        input_matrix = np.loadtxt(filepath, skiprows=1)
-    else
-        input_matrix = np.loadtxt(filepath)
+
+
+
+# Create Materials
+materials = Vector{Material}()
+push!(materials, Material(1, 1., 0.2, 75))
+push!(materials, Material(2, 1., 0.2, 76))
+println("Created ", length(materials), " materials!")
+
+# Create Nodes (this process allows the nodes and position vector of vectors to point to the same thing)
+positions = Vector{MVector{3, Float64}}()
+push!(positions, [0.0, 0.0, 0.0])
+push!(positions, [1.0, 0.0, 0.0])
+
+nodes = Vector{Node}()
+push!(nodes, Node(positions[1],materials[1]))
+push!(nodes, Node(positions[2],materials[2]))
+println("Created ", length(nodes), " nodes!")
+
+
+# Create Bonds (double bonded)
+bonds = Vector{Bond}()
+tree = NearestNeighbors.BallTree(Vector{SVector{3, Float64}}(positions))
+results = NearestNeighbors.inrange(tree, positions, horizon, false)
+for (i, result) in enumerate(results)
+    for neighbor in result
+        if i != neighbor
+            push!(bonds, Bond(nodes[i], nodes[neighbor], false))
+        end
     end
-    
-    print(size(input_matrix))
 end
-
-
-function legacy_parse_grid(filepath::String)
-    np = PyCall.pyimport("numpy")
-    # Read in grid using numpy
-    input_matrix = np.loadtxt(filepath, skiprows=1)
-
-    # Initial positions are the first 3 columns in the matrix
-    positions = input_matrix[:,1:3]
-    displacements = zeros(size(positions))
-    velocities = zeros(size(positions))
-    forces = zeros(size(positions))
-
-    # Material association numbers are last (4th) column in the matrix
-    material_numbers = convert(Vector{Int32}, input_matrix[:,4])
-    mat::Material = Material(1.,0.1,10000000,2.,2*3.015)
-    nodes = []
-    for i in 1:size(positions)[1]
-        # n::Node = 
-        append!(nodes,[Node(positions[i,:], displacements[i,:], velocities[i,:], forces[i,:], mat)])
-    end
-
-    return nodes
-end
-
-
-
-
-println("Data structure sizes:")
-println("\tBond Size: ",        sizeof(Bond))
-println("\tMaterial Size: ",    sizeof(Material))
-println("\tNode Size: ",        sizeof(Node))
-println("\tGrid Size: ",        sizeof(Grid), "\n\n")
-
-# println("Nodes:\n",legacy_parse_grid("testgrid.grid"))
-parse_grid("testgrid.grid", true)
-# np = PyCall.pyimport("numpy")
-# input_matrix = np.loadtxt("testgrid.grid", skiprows=1)
-
-# println(typeof(arr))
-
-
-# Read input file - materials, boundary conditions
-    # Material
-    # BC
-        # Needs: Grid, Name(?), Type(?), capture criteria (inside volume, outside volume, crossing surface, etc.)
-        # Types
-            # Bond BC
-                # Force Plane
-            # Node BC
-                # Displacement, Velocity, Acceleration, Staged Loading, etc
-        # Pass it the grid and it handles everything?
-
-# Read grid file - per-node properties (volume, position, displacement, nodal material, etc)
-
-
-
-
-# Create materials
-
-# Create nodes
-
-# Create bonds
-
-# Create boundary conditions
-
+println("Created ", length(bonds), " bonds!")
 
 # Time iteration
+node_dispalcement = Vector{Float64}() # used to record a node's dispalcement
+nodes[2].displacement[1] = 0.05
+nodes[1].displacement[1] = -0.05
+# nodes[2].force[1] = 0.1
+dt = grid_spacing / sqrt(maximum([material.bond_constant/material.density for material in materials])) * 0.01
+for time_step in 1:1000000
+    for node in nodes
+        # Average velocity for the time step assuming constant acceleration
+        node.velocity = node.velocity + (dt*0.5)*(node.force/(node.volume*node.material.density))
+
+        # Update displacement with average velocity
+        node.displacement = node.displacement + node.velocity * dt
+    end
+    # Apply displacement BCs
+    # nodes[1].displacement[1] = 0.
+    # Calculate forces
+    for bond in bonds
+        # Eventually replace node.force with atomic floats and use threading for this loop
+        apply_force(bond)
+    end
+    for node in nodes
+        # Calculate final velocity
+        node.velocity = node.velocity + dt*0.5 * (node.force / (node.volume * node.material.density))
+    end
+    push!(node_dispalcement, nodes[2].displacement[1])
+end
+
+print(10000*dt)
+using Plots
+plot(node_dispalcement)
