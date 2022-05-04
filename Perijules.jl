3eadd2e540334140778c212502b1067d40484e42
watsonkh <watsonkh@vcu.edu>
2022-05-04 19:09:10 -0400

    basic force plane

--- Perijules.jl
+++ Perijules.jl
@@ -4,6 +4,7 @@ Pkg.activate(".")
 import NearestNeighbors
 using StaticArrays
 using LinearAlgebra
+import PyCall
 
 # Global (for now) varibles
 grid_spacing = 1.
@@ -19,54 +20,72 @@ end
 
 # TOOD for tensile bundle:
     # Nonlinear force response
-    # Reading in and creating materials
     # Material interface
     # Staged loading BC
-    # Force plane
+
+
 
 
 # Initialize grid
-# Create Materials
+
+# Read in input grid
+np = PyCall.pyimport("numpy")
+data = np.loadtxt("testgrid.grid", skiprows=1)
+
+# Create materials
+material_numbers = Vector{Int64}(data[:,4])
+unique_material_numbers = sort(unique!(Vector{Int64}(data[:,4])))
 materials = Vector{PD.Material}()
-push!(materials, PD.Material(1, 1., 0.1, 75))
-push!(materials, PD.Material(2, 1., 0.1, 76))
-println("Created ", length(materials), " materials!")
+for material_number in unique_material_numbers
+    push!(materials, PD.Material(material_number, 1., 0.1, 75))
+end
 
 # Create Nodes (this process allows the nodes and position vector of vectors to point to the same thing)
-positions = Vector{MVector{3, Float64}}()
-push!(positions, [0.0, 0.0, 0.0])
-push!(positions, [1.0, 0.0, 0.0])
-
+positions = data[:,1:3]
 nodes = Vector{PD.Node}()
-push!(nodes, PD.Node(positions[1],materials[1],grid_spacing))
-push!(nodes, PD.Node(positions[2],materials[2],grid_spacing))
-println("Created ", length(nodes), " nodes!")
+for (i, position) in enumerate(eachrow(positions))
+    material = [material for material in materials if material.id == material_numbers[i]][1]
+    push!(nodes, PD.Node(MVector{3, Float64}(position), material, grid_spacing))
+end
 
+println("Created ", length(nodes), " nodes!")
 
 # Create Bonds (double bonded)
 bonds = Vector{PD.Bond}()
-tree = NearestNeighbors.BallTree(Vector{SVector{3, Float64}}(positions))
-results = NearestNeighbors.inrange(tree, positions, horizon, false)
-for (i, result) in enumerate(results)
+spatial = PyCall.pyimport_conda("scipy.spatial", "scipy", "anaconda")
+tree = spatial.KDTree(positions);
+py_neighbor_list = tree.query_ball_point(positions, horizon, workers=-1);
+neighbor_list = Vector{Vector{Int64}}(py_neighbor_list)
+
+for (i, result) in enumerate(neighbor_list)
     for neighbor in result
-        if i != neighbor
-            push!(bonds, PD.Bond(nodes[i], nodes[neighbor], false))
+        n = neighbor+1
+        if i != n
+            push!(bonds, PD.Bond(nodes[i], nodes[n], false))
         end
     end
 end
 println("Created ", length(bonds), " bonds!")
 
+## Plot initial grid
+pv = PyCall.pyimport_conda("pyvista", "pyvista", "conda-forge")
+point_cloud = pv.PolyData([node.position for node in nodes])
+plotter = pv.Plotter()
+plotter.add_mesh(point_cloud, scalars=material_numbers, cmap="plasma")
+plotter.show()
+##
 
+SL_negative_points = [node for node in nodes if node.position[1] < -950]
+SL_positive_points = [node for node in nodes if node.position[1] > 950]
 
+FP_positive_bonds = [bond for bond in bonds if (bond.from.position[1] < 0 && bond.to.position[1] > 0)]
+FP_negative_bonds = [bond for bond in bonds if (bond.from.position[1] > 0 && bond.to.position[1] < 0)]
+FP_his = Vector{Float64}()
 
-
+##
 
 # Time iteration
-node_a_position = Vector{Float64}() # used to record a node's dispalcement
-node_b_position = Vector{Float64}() # used to record b node's dispalcement
 ke_his = Vector{Float64}() # used to record kinetic energy
-
-nodes[2].velocity[1] = 0.5
 
 dt = grid_spacing / sqrt(maximum([material.bond_constant/material.density for material in materials])) * 0.01
 for time_step in 1:1000
@@ -81,10 +100,6 @@ for time_step in 1:1000
         @atomic node.force = zeros(3)
     end
 
-    # Apply displacement BCs
-    nodes[1].displacement[1] = 0.
-    nodes[1].velocity[1] = 0.
-
     # Calculate forces and break bonds
     Threads.@threads for bond in bonds
         if PD.should_break(bond)
@@ -94,17 +109,20 @@ for time_step in 1:1000
         PD.apply_force(bond)
     end
 
+    push!(FP_his, sum([PD.get_force(bond) for bond in FP_positive_bonds]) -
+     sum([PD.get_force(bond) for bond in FP_negative_bonds]))
+
     Threads.@threads for node in nodes
         # Calculate final velocity
         node.velocity = node.velocity + dt*0.5 * (node.force / (node.volume * node.material.density))
     end
 
-    push!(node_a_position, nodes[1].position[1] + nodes[1].displacement[1])
-    push!(node_b_position, nodes[2].position[1] + nodes[2].displacement[1])
-    push!(ke_his,sum([0.5 * node.volume * node.material.density * (norm(node.velocity)^2) for node in nodes]))
+    kinetic_energy = sum([0.5 * node.volume * node.material.density * (norm(node.velocity)^2) for node in nodes])
+    push!(ke_his, sum([0.5 * node.volume * node.material.density * (norm(node.velocity)^2) for node in nodes]))
+    println(time_step)
 end
 println("Finished time loop!")
 
 # Plot the two points' deformed position
 import Plots
-Plots.plot([node_a_position,node_b_position, ke_his], label=["node a" "node b" "kinetic energy"])
+Plots.plot(ke_his, label=["kinetic energy"])
